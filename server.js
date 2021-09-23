ef84937c741bd5850d0f4db0a19b9f8ad5503758
yjk007 <yjk007@ucsd.edu>
2021-09-23 16:38:37 -0700

    added DB

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true
+});
 
-const indexRouter = require('./routes/index')
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+const indexRouter = require('./routes/index');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
