fb9aa72d01bac2d584794aab7b489e05b27a19a7
yjk007 <yjk007@ucsd.edu>
2021-09-23 18:48:58 -0700

    Finished setting up species

---
+++ models/species.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const speciesSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required:true
+    }
+});
+
+
+module.exports = mongoose.model('Species', speciesSchema);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect(process.env.DATABASE_URL, { 
     useNewUrlParser: true
@@ -17,13 +18,18 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 const indexRouter = require('./routes/index');
+const speciesRouter = require('./routes/species');
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 app.use('/', indexRouter);
+app.use('/species', speciesRouter);
+
 
 app.listen(process.env.PORT || 3000);
